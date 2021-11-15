7007bd1121ca31d3508fd1abf7a8a052e31f1918
Александр Моль <sashamiasko@gmail.com>
2021-11-15 07:29:48 +0700

    saving My Cool App

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+function Header (){
+    return(
+        <header>
+            <h1>It is my COOL app! Suka Blyat!</h1>
+        </header>
+    );
+}
+export default Header;
