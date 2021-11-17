c971048ff99851f4caac74abb332a6d94af85a51
Александр Моль <sashamiasko@gmail.com>
2021-11-17 08:26:10 +0700

    homework is done

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import styles from './header.less';
 
 function Header (){
     return(
         <header>
-            <h1>It is my COOL app! Suka Blyat!</h1>
+            <h1 className={styles.example}>Hello React!!!</h1>
         </header>
     );
 }
