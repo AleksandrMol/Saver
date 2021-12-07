6befa5697a801fb028daf940c03f4b960f44896f
Александр Моль <sashamiasko@gmail.com>
2021-12-07 12:15:28 +0700

    homework is done

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const { realpath } = require('fs');
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV == 'development';
 const IS_PROD = NODE_ENV == 'production';
+const GLOBAL_CSS_REGEXP = /\.global\.css$/;
 
 function setupDevtool () {
     if (IS_DEV) return 'eval';
@@ -28,7 +29,7 @@ module.exports = {
                 use: ['ts-loader']
             },
             {
-                test:/\.less$/,
+                test:/\.css$/,
                 use: [
                     'style-loader', {
                         loader: 'css-loader',
@@ -39,9 +40,28 @@ module.exports = {
                             },
                         }
                     },
-                    'less-loader',
-                ]
-            }
+                ],
+                exclude: GLOBAL_CSS_REGEXP
+            },
+            {
+                test: GLOBAL_CSS_REGEXP,
+                use: ['style-loader', 'css-loader'],
+            },
+            // {
+            //     test:/\.less$/,
+            //     use: [
+            //         'style-loader', {
+            //             loader: 'css-loader',
+            //             options: {
+            //                 modules: {
+            //                     mode:'local',
+            //                     localIdentName:'[name]__[local]--[hash:base64:5]'
+            //                 },
+            //             }
+            //         },
+            //         'less-loader',
+            //     ]
+            // },
         ]
     },
     plugins: [
@@ -50,7 +70,6 @@ module.exports = {
     devServer: {
         port: 3000,
         open: true,
-        // historyApiFallback: true,
         hot: IS_DEV    
     },
     devtool: setupDevtool(),
